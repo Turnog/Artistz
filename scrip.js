0464701ec0a4bb06c69a3bea3ec1af8e2e2876da
Phuoc Truong <ndphuoctruong@gmail.com>
2024-07-20 11:47:59 +0700

    Artistz

---
+++ scrip.js
@@ -0,0 +1,20 @@
+window.onscroll = function() {myfunction()};
+
+var navBar  = document.getElementById('navbar');
+var sticky = navBar.offsetTop;
+
+function myfunction(){
+    if(window.pageYOffset >= sticky){
+        navBar.classList.add('sticky');
+    }
+    else{
+        navBar.classList.remove("sticky");
+    }
+}
+
+let menu = document.querySelector('#menuBar');
+let navbar = document.querySelector('.navmenu');
+
+menu.onclick = () => {
+    navbar.classList.toggle('open');
+}
